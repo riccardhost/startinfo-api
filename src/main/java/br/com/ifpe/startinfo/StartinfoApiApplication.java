053b9fae5a1d1bf449e9451e66a467ca594a9a2b
Ricardo Santos <riccardhos@gmail.com>
2025-01-19 13:29:38 -0300

    Projeto API: Start Info - Letramento Digital de Idosos

---
+++ src/main/java/br/com/ifpe/startinfo/StartinfoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.startinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartinfoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartinfoApiApplication.class, args);
+	}
+
+}
